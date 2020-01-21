fa73d4a3e3c080bad0182310bc4a80de48c4278a
Allan <allanlumumba@gmail.com>
2020-01-21 15:10:13 +0300

    add five array quotes

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -11,7 +11,7 @@ export class QuoteComponent implements OnInit {
   quotes:Quote[]=  [
     new Quote(1, 'I believe you can speak things into existence.', 'Jay Z',new Date(2018,1,2)),
     new Quote(2,"Don't be afraid to give up the good to go for the great.",'John D. Rockerfeller',new Date(2017,6,8)),
-    new Quote(3,'Regardless of how it goes down, life goes on','Rick Ross',new Date(2019,1,12)),
+    new Quote(3,'Regardless of how it goes down, life goes on','Rick Ross',new Date(2019,1,11)),
     new Quote(4,'We make a living by what we get, but we make a life by what we give.','Winston Churchill',new Date(2019,2,19)),
     new Quote(5,'Study hard so that you can master technology, which allows us to master nature.','Che Guevara',new Date(2019,4,20)),
     new Quote(6,'The reason children accept discpline from their parents is because they know their parents love them.','Nipsey Hussle',new Date(2018,1,1)),
